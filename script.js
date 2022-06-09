b74072d88f4097b2d5e736332f5c2c5b6ca34b69
James <JamesPGrant1999@gmail.com>
2022-06-09 04:28:12 -0400

    need to fix some things

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
 let gameBoard = [];
 let players = [];
-let win = [];
+let player1Taken = [];
+let player2Taken = []
+let win = [
+    [`1`,`2`,`3`],
+    [`4`,`5`,`6`],
+    [`7`,`8`,`9`],
+    [`1`,`5`,`9`],
+    [`1`,`4`,`7`],
+    [`7`,`4`,`1`],
+    [`2`,`5`,`8`],
+    [`3`,`5`,`7`]
+];
+let values = []
 let playerCurrent = ``
 const board = document.querySelector(`.board`);
 const X = document.querySelector(`.X`)
 const O = document.querySelector(`.O`)
 const buttons = document.querySelectorAll(`.buttonXO`)
+const RESETBUTTON = document.createElement(`button`)
+document.body.append(RESETBUTTON);
+RESETBUTTON.textContent = `RESET`
+RESETBUTTON.style.display = `none`
+const winner = document.createElement(`div`);
+document.body.appendChild(winner)
+const whoWon = document.createElement(`p`);
 
 //123 123 123 321 111 222 333 
 //add classes for each square like 1, 2 , 3 , 4, 5, 6, 7, 8,9
+//array push each square taken then every array method then.includes
 const player = (choice, name) =>{
     const getChoice = () => choice;
     const getName = () => name;
     const whoLost = () =>{
-        win.forEach( function(i){
-            const squares = document.querySelector(`[value = "${i}"]`)
-            const player1 = players[0];
-            const player2 = players[1];
-            if(squares.id === player1.choice ){
-                if(players.choice === 'X' squares.value){
-                    console.log('you won!')
-                }
+     for(let i = 0; i < win.length; i++){
+         const squares = document.querySelectorAll(`.squares`)
+            if(win[i].every(ele =>(player1Taken).includes(ele))){
+                winner.appendChild(whoWon);
+                whoWon.textContent = `${name} won!`
+                console.log('you won!')       
+            } else if(win[i].every(ele =>(player2Taken).includes(ele))){
+                winner.appendChild(whoWon);
+                whoWon.textContent = `${name} won!`
+                console.log(`i won!`)
+            } else if(squares.forEach(square => square.id !== ``) && 
+            win[i].every(ele => !player2Taken.includes(ele) &&
+            win[i].every(ele => !player1Taken.includes(ele)))){
+                winner.appendChild(whoWon);
+                whoWon.textContent = `Its a Tie!`
             }
-        })
+        }
     }
-    return { choice:getChoice(), name:getName(), whoLost};
+    return { choice:getChoice(), name:getName(), whoLost };
 
 }
 const GAMEBOARD = (() =>{
@@ -33,7 +60,7 @@ const GAMEBOARD = (() =>{
             const squares = document.createElement(`div`);
             squares.classList.add('squares');
             squares.setAttribute(`value`, `${i}`);
-            win.push(i);
+            values.push(i);
             board.appendChild(squares);
             console.log(win)
             }
@@ -47,8 +74,8 @@ GAMEBOARD.populate()
 
 function userChoice(){
     X.addEventListener('click', ()=>{
-        const player1 = player(`X`, 'player1')
-        const player2 = player(`O`, `player2`)
+        const player1 = player(`X`, 'Player1')
+        const player2 = player(`O`, `Player2`)
         players.push(player1)
         players.push(player2)
     
@@ -82,21 +109,44 @@ function XO(e, currentPlayer){
     squares.forEach(square => square.addEventListener('click', (e) => {
         const player1 = players[0]
         const player2 = players[1]
+        if(whoWon.textContent !== ``){
+            RESETBUTTON.style.display = `block`
+            RESETBUTTON.addEventListener(`click`, ()=>{
+                window.location.reload()
+            })
+            return
+        }
          if( playerCurrent === ``){
              playerCurrent = player1
+             if(square.id === player2.choice ){
+                return
+            }
             square.setAttribute(`id`, `${player1.choice}`)
             square.textContent = `${player1.choice} `
+            player1Taken.push(square.getAttribute(`value`))
             gameBoard.push(player1.choice)
-            console.log(gameBoard)
+            player1.whoLost()
             return playerCurrent = player2
          } else if( playerCurrent=== player2 ){
+            if(square.id === player1.choice ){
+                return
+            }
             square.setAttribute(`id`, `${player2.choice}`)
             square.textContent = `${player2.choice} `
+            player2Taken.push(square.getAttribute(`value`))
             gameBoard.push(player2.choice)
             player2.whoLost()
-            console.log(gameBoard)
             return playerCurrent = ``  
         }
     }))
+
 }
 XO()
+
+
+/*             if(squares.id === player1.choice || squares.id === player2.choice){
+                if(squares.getAttribute('value') === `1` &&squares.id ===player1.choice && squares.getAttribute('value') === `2`  && squares.id ===player1.choice
+                && squares.getAttribute('value') === `3`&& squares.id ===player1.choice || 
+                squares.getAttribute('value') === [`1`,  `2`, `3`]  && squares.id ===player1.choice ){
+                    console.log('you won!')
+                } */
